1e8017044e526bb6efed81b11027b2e18e7d94b5
PC-201712171719\Administrator <275702919@qq.com>
2018-06-21 23:31:42 +0800

    no message

--- serverTest2/serverTest2/udptest.cpp
+++ serverTest2/serverTest2/udptest.cpp
@@ -24,7 +24,7 @@ int main()
 
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 	sockServer = socket(AF_INET, SOCK_DGRAM, 0);
-	addrServer.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//INADDR_ANY表示任何IP
+	addrServer.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//INADDR_ANY表示任何IP1111
 	addrServer.sin_family = AF_INET;
 	addrServer.sin_port = htons(6000);//绑定端口6000
 	nRet = bind(sockServer, (SOCKADDR*)&addrServer, sizeof(SOCKADDR));
